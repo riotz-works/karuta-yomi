90f2785922662bcd94f6c611bb5a1771ab31880a
lulzneko <lulzneko@riotz.works>
2020-04-07 14:36:22 +0900

    Add vuetify for material component framework of Vue.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
+import Vuetify from 'vuetify/lib';
+import '~/assets/style/global.scss';
 import DefaultLayout from '~/layouts/Default.vue';
 
-export default function(Vue) {
+export default function(Vue, { appOptions }) {
   Vue.component('Layout', DefaultLayout);
+
+  Vue.use(Vuetify);
+  appOptions.vuetify = new Vuetify({});
 }
