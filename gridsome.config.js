4afe3a78842790430de82b67b123dd46cf69e248
lulzneko <lulzneko@riotz.works>
2020-04-07 12:00:44 +0900

    Add hard-source-webpack-plugin for provide an intermediate caching

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,8 +1,18 @@
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const { displayName, description } = require('./package.json');
+
+const production = process.env.NODE_ENV === 'production';
+
 
 module.exports = {
   port: 3000,
 
   siteName: displayName,
-  siteDescription: description
+  siteDescription: description,
+
+  configureWebpack: {
+    plugins: [
+      ...production ? [] : [ new HardSourceWebpackPlugin() ]
+    ]
+  }
 };
