4530fe72109310b9b350cb970f1beddc8517d1d3
lulzneko <lulzneko@riotz.works>
2020-04-07 14:36:22 +0900

    Add vuetify for material component framework of Vue.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-process-env */                 // 'cuz configuration file and requires environment variables
 /* eslint-disable @typescript-eslint/camelcase */   // 'cuz key name of configuration is specified
+const Fiber = require('fibers');
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 const { displayName, description, name } = require('./package.json');
 
 const production = process.env.NODE_ENV === 'production';
@@ -13,6 +15,17 @@ module.exports = {
   siteName: displayName,
   siteDescription: description,
   siteUrl: `https://riotz.works/${name}`,
+
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: '@import "~/assets/style/variables.scss"',
+        sassOptions: {
+          fiber: Fiber
+        }
+      }
+    }
+  },
 
   plugins: [
     { use: '@gridsome/plugin-google-analytics', options: { id: 'UA-109297390-1' }},
@@ -27,6 +40,7 @@ module.exports = {
       ]
     },
     plugins: [
+      new VuetifyLoaderPlugin(),
       ...production ? [] : [ new HardSourceWebpackPlugin() ]
     ],
     optimization: {
