6a261cad077808ef0ad544e623fec9d3eb60d185
lulzneko <lulzneko@riotz.works>
2020-04-07 14:54:33 +0900

    Introduce basic layout and global styles

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+import pkg from '../package.json';
+
+export const Consts = Object.freeze({
+  NAME: pkg.name,
+  DISPLAY_NAME: pkg.displayName,
+  DESCRIPTION: pkg.description,
+  VERSION: pkg.version,
+
+  INCEPTION_YEAR: 2020
+});
