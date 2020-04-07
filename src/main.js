b8c0039b6b74a09b0db76a1d6bc9f342f5c6da65
lulzneko <lulzneko@riotz.works>
2020-04-07 14:37:45 +0900

    Introduce custom icons with global definitions

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@ import Vuetify from 'vuetify/lib';
 import '~/assets/style/global.scss';
 import DefaultLayout from '~/layouts/Default.vue';
 
-export default function(Vue, { appOptions }) {
+export default function(Vue, { appOptions, head }) {
   Vue.component('Layout', DefaultLayout);
 
+  head.link.push({ rel: 'stylesheet', href: 'https://unicons.iconscout.com/release/v2.1.3/css/unicons.css' });
+
   Vue.use(Vuetify);
-  appOptions.vuetify = new Vuetify({});
+  appOptions.vuetify = new Vuetify({
+    icons: {
+      values: {
+        home: 'uil-home-alt',
+        about: 'uil-info-circle'
+      }
+    }
+  });
 }
