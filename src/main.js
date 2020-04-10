b14f88213c5c2bc304dec0a4ae327b5fcd45715f
lulzneko <lulzneko@riotz.works>
2020-04-11 00:33:17 +0900

    Introduce web fonts and definitions to use in header title and body text

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import DefaultLayout from '~/layouts/Default.vue';
 export default function(Vue, { appOptions, head }) {
   Vue.component('Layout', DefaultLayout);
 
+  head.link.push({ rel: 'stylesheet', href: 'https://fonts.googleapis.com/earlyaccess/hannari.css' });
+  head.link.push({ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Quicksand&display=swap' });
+  head.link.push({ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Sawarabi+Mincho&display=swap' });
+  head.link.push({ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap' });
   head.link.push({ rel: 'stylesheet', href: 'https://unicons.iconscout.com/release/v2.1.3/css/unicons.css' });
 
   Vue.use(Vuetify);
